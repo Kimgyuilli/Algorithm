81720452a16aa6203023bd7fc87c1be2c9d14134
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2025-04-10 01:55:12 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 612 ms, Memory: 56692 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,80 +1,87 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    private final static int[] DX = { 1, 0, -1, 0 };
-    private final static int[] DY = { 0, -1, 0, 1 };
-    private static int[][] map, distance;
-    private static int m, n;
+    static int[][] arr, distance;
+    static int N, M;
+    private final static int[] DY = { -1, 0, 1, 0 }; // row
+    private final static int[] DX = { 0, -1, 0, 1 }; // col
     private static boolean[][] isVisited;
-    
+    public static boolean isStartChecked = false;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder builder = new StringBuilder();
-        boolean isStartChecked = false;
-        String[] size = reader.readLine().split(" ");
-        n = Integer.parseInt(size[0]);
-        m = Integer.parseInt(size[1]);
-        int startX = -1, startY = -1;
-        
-        map = new int[n][m];
-        distance = new int[n][m];
-        isVisited = new boolean[n][m];
-        
-        for (int i = 0; i < n; i++) {
-            map[i] = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            if (!isStartChecked) 
-                for (int j = 0; j < m; j++) 
-                    if (map[i][j] == 2) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int startX = 0, startY = 0;
+
+        arr = new int[N][M];
+        distance = new int[N][M];
+        isVisited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            if (!isStartChecked)
+                for (int j = 0; j < M; j++)
+                    if (arr[i][j] == 2) {
                         isStartChecked = true;
                         startX = i;
                         startY = j;
                         break;
                     }
         }
-        
         bfs(startX, startY);
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) 
-                if (!isVisited[i][j] && map[i][j] == 1)
-                    builder.append(-1 + " ");
-                else 
-                    builder.append(distance[i][j] + " ");
-            builder.append("\n");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++)
+                if (!isVisited[i][j] && arr[i][j] == 1)
+                    sb.append(-1 + " ");
+                else
+                    sb.append(distance[i][j]).append(" ");
+            sb.append("\n");
         }
-        
-        System.out.print(builder.toString());
+        System.out.println(sb);
+
     }
-    
     private static void bfs(int x, int y) {
         Queue<Point> queue = new LinkedList<>();
         queue.add(new Point(x, y));
         isVisited[x][y] = true;
-        
+
         while (!queue.isEmpty()) {
             Point current = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nextX = current.x + DX[i];
-                int nextY = current.y + DY[i];
-                
-                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
-                if (map[nextX][nextY] == 0) continue;
+                int nextX = current.row + DX[i];
+                int nextY = current.col + DY[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
+                if (arr[nextX][nextY] == 0) continue;
                 if (isVisited[nextX][nextY]) continue;
 
                 queue.add(new Point(nextX, nextY));
-                distance[nextX][nextY] = distance[current.x][current.y] + 1;
+                distance[nextX][nextY] = distance[current.row][current.col] + 1;
                 isVisited[nextX][nextY] = true;
             }
         }
     }
-}
 
-class Point {
-    public int x, y;
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+
+    static class Point {
+        int row, col;
+        public Point(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
     }
+
 }
