b09b76cc641c212dc3f73f0f5c79f6984356bf43
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2025-05-16 15:57:10 +0900

    [Gold V] Title: 틱택토, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.java
+++ 백준/Gold/7682. 틱택토/틱택토.java
@@ -5,74 +5,78 @@ public class Main {
     static int Xcount, Ocount;
     static char[][] map = new char[3][3];
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
 
         while (true) {
             String input = br.readLine();
+            if (input.equals("end")) break;
+            if (input.length() != 9) {
+                System.out.println("invalid");
+                continue;
+            }
+
             Xcount = 0;
             Ocount = 0;
-            for(int i = 0; i < 3 ; i++) {
-                for(int j = 0; j < 3; j++) {
-                    map[i][j] = input.charAt(i * 3 + j);
-                    if(map[i][j] == 'X') {
-                        Xcount++;
-                    } else if (map[i][j] == 'O') {
-                        Ocount++;
-                    } else if(map[i][j] == '.') {
-                        map[i][j] = '.';
-                    } else {
-                        return;
+
+            // 보드 구성 및 개수 세기
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    char c = input.charAt(i * 3 + j);
+                    if (c == 'X') Xcount++;
+                    else if (c == 'O') Ocount++;
+                    else if (c != '.') {
+                        System.out.println("invalid");
+                        continue;
                     }
+                    map[i][j] = c;
                 }
             }
+
             boolean Xwin = isWin('X');
             boolean Owin = isWin('O');
 
-            if(Xcount < Ocount){
+            // 불가능한 수 차이
+            if (Xcount < Ocount || Xcount > Ocount + 1) {
                 System.out.println("invalid");
                 continue;
             }
-            if(Xwin && Owin) {
-                System.out.println("invalid");
-                continue;
-            } else if (!Xwin && !Owin && Xcount + Ocount < 9) {
-                System.out.println("invalid");
-                continue;
-            }
-            if(Xwin && Xcount != Ocount + 1) {
-                System.out.println("invalid");
-                continue;
-            }
-            if(Owin && Xcount != Ocount) {
-                System.out.println("invalid");
-                continue;
-            }
-            System.out.println("valid");
 
+            // 둘 다 승리하면 안 됨
+            if (Xwin && Owin) {
+                System.out.println("invalid");
+                continue;
+            }
+
+            // 승리 조건에 맞지 않는 수 차이
+            if (Xwin && Xcount != Ocount + 1) {
+                System.out.println("invalid");
+                continue;
+            }
+
+            if (Owin && Xcount != Ocount) {
+                System.out.println("invalid");
+                continue;
+            }
+
+            // 아무도 안 이기고 판도 안 찼으면 진행 중인 게임 (invalid)
+            if (!Xwin && !Owin && Xcount + Ocount < 9) {
+                System.out.println("invalid");
+                continue;
+            }
+
+            // 유효한 종료 상태
+            System.out.println("valid");
         }
     }
 
     static boolean isWin(char player) {
-        for(int i = 0; i < 3; i++) {
-            if (map[i][0] == player && map[i][1] == player && map[i][2] == player) {
-                return true;
-            }
+        for (int i = 0; i < 3; i++) {
+            if (map[i][0] == player && map[i][1] == player && map[i][2] == player) return true;
+            if (map[0][i] == player && map[1][i] == player && map[2][i] == player) return true;
         }
-        for(int i = 0; i < 3; i++) {
-            if (map[0][i] == player && map[1][i] == player && map[2][i] == player) {
-                return true;
-            }
-        }
-        if (map[0][0] == player && map[1][1] == player && map[2][2] == player) {
-            return true;
-        }
-        if (map[0][2] == player && map[1][1] == player && map[2][0] == player) {
-            return true;
-        }
+        if (map[0][0] == player && map[1][1] == player && map[2][2] == player) return true;
+        if (map[0][2] == player && map[1][1] == player && map[2][0] == player) return true;
         return false;
-
     }
-
 }
