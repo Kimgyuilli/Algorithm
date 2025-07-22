9f56ecf683ecf86e991f8694b26f85842720c072
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2025-07-22 14:29:19 +0900

    [Bronze II] Title: 16진수, Time: 96 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int n;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine(), 16);
+        System.out.println(n);
+    }
+
+}
