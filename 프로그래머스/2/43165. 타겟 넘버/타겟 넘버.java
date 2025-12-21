cc027e956869c990be9e918a680c0855aafee99c
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2025-12-22 01:02:09 +0900

    [level 2] Title: 타겟 넘버, Time: 5.76 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,24 @@
+class Solution {
+    static int answer;
+    public int solution(int[] numbers, int target) {
+        
+        answer = 0;
+         
+        dfs(0, 0, numbers, target);
+        
+        
+        return answer;
+    }
+    
+    private void dfs(int depth, int sum, int[] numbers, int target){
+        if(depth == numbers.length){
+            if(sum == target){
+                answer += 1;
+            }
+            return;
+        }
+
+        dfs(depth + 1, sum + numbers[depth], numbers, target);
+        dfs(depth + 1, sum - numbers[depth], numbers, target);
+    }
+}
