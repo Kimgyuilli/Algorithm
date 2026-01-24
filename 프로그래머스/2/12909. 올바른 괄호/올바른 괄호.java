aa8e6b941a41728c956fff370b5ee40482d65243
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-01-24 18:49:16 +0900

    [level 2] Title: 올바른 괄호, Time: 7.85 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,22 @@
-import java.util.Stack;
-
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        int count = 0;
+
         for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(c == '('){
-                stack.push(c);
+            if(s.charAt(i) == '('){
+                count++;
             }
-            else if(c == ')'){
-                if(stack.isEmpty()){
+            
+            if(s.charAt(i) == ')'){
+                if(count == 0){
                     return false;
                 }
-                stack.pop();
+                else{
+                    count--;
+                }
             }
         }
-        return stack.isEmpty();
+
+        return count == 0;
     }
 }
