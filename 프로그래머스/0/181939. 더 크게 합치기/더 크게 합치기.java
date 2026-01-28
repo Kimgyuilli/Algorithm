adba37182e51df8317b57e6a526b0417a24dafdd
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-01-29 00:42:19 +0900

    [level 0] Title: 더 크게 합치기, Time: 11.55 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int a, int b) {
+        
+        int i1 = Integer.parseInt(a + "" + b);
+        int i2 = Integer.parseInt(b + "" + a);
+        
+        return i1 < i2 ? i2 : i1;
+    }
+}
