d29699f2fc7d2c782af25ca3f5d3d44c53325baf
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-02-01 16:55:44 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 3.38 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        Arrays.sort(num_list);
+        
+        return Arrays.stream(num_list)
+            .limit(5)
+            .toArray();
+    }
+}
