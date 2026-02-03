88b4e1048e7ac9ff71f0ec9b85f568f964c8adb3
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-02-04 01:58:48 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0, 0};
+        
+        int maxX = board[0] / 2;
+        int maxY = board[1] / 2;
+        
+        for(String input : keyinput){
+            switch(input){
+                case "left":
+                    if(answer[0] > -maxX) answer[0]--;
+                    break;
+                case "right":
+                    if(answer[0] < maxX) answer[0]++;
+                    break;
+                case "up":
+                    if(answer[1] < maxY) answer[1]++;
+                    break;
+                case "down":
+                    if(answer[1] > -maxY) answer[1]--;
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
