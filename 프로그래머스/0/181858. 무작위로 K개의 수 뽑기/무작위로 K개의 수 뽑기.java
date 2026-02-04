3f9c018329c0097bcb5f3a19cb4261150841b8e4
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-02-05 02:23:08 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.60 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[k];
+        Arrays.fill(answer, -1);
+        
+        Set<Integer> seen = new HashSet<>();
+        int idx = 0;
+        
+        for(int num : arr){
+            if(!seen.contains(num)){
+                answer[idx++] = num;
+                seen.add(num);
+                if(idx == k) break;
+            }
+        }
+        
+        return answer;
+    }
+}
