53304869b2b0eeab51a9e9f1215ca4c6d64fe175
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-02-10 09:14:18 +0900

    [level 1] Title: 폰켓몬, Time: 5.69 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> types = new HashSet<>();
+        for(int num : nums) {
+            types.add(num);
+        }
+        
+        return Math.min(types.size(), nums.length / 2);
+    }
+}
