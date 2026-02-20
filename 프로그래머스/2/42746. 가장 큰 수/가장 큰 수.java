6223ca596e110065c3e536fd81d516c186393f09
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-02-20 09:56:41 +0900

    [level 2] Title: 가장 큰 수, Time: 242.02 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strNums = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+
+        Arrays.sort(strNums, (a, b) -> (b + a).compareTo(a + b));
+
+        if (strNums[0].equals("0")) return "0";
+
+        StringBuilder answer = new StringBuilder();
+        for (String s : strNums) {
+            answer.append(s);
+        }
+
+        return answer.toString();
+    }
+}
