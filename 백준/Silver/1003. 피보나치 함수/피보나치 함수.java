a97862e84d8687d3688e0ab72efb78df1231a32d
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-03-03 17:32:47 +0900

    [Silver III] Title: 피보나치 함수, Time: 128 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,34 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     static Integer[][] dp = new Integer[41][2];
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         dp[0][0] = 1;
         dp[0][1] = 0;
         dp[1][0] = 0;
-        dp[1][1] = 1;	
+        dp[1][1] = 1;
 
-        int T = in.nextInt();
+        int T = Integer.parseInt(br.readLine());
 
         while(T-- > 0){
-            int N = in.nextInt();
+            int N = Integer.parseInt(br.readLine());
             fibonacci(N);
             System.out.println(dp[N][0] + " " + dp[N][1]);
         }
 
     }
 
-    static Integer[] fibonacci(int N) {
-        if(dp[N][0] == null || dp[N][1] == null) {
-            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
-            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+    public static Integer[] fibonacci(int n) {
+        if(dp[n][0] == null || dp[n][1] == null) {
+            dp[n][0] = fibonacci(n-1)[0] + fibonacci(n-2)[0];
+            dp[n][1] = fibonacci(n-1)[1] + fibonacci(n-2)[1];
         }
-        return dp[N];
-
+        return dp[n];
     }
-
 }
