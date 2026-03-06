863aa252bee684b94a8222d662ba0863da9701e4
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-03-06 22:02:54 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -1,29 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, long k) {
-        List<Integer> li = new ArrayList<>();
-        long total = 1;
-        
-        for (int i = 1; i <= n; i++) {
-            total *= i;
-            li.add(i);
-        }
-        
-        k--; 
-        
-        int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            total /= (n - i); 
-            
-            int index = (int) (k / total);
-            
-            answer[i] = li.get(index);
-            li.remove(index);
-            
-            k %= total;
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
