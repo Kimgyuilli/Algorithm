db2659299d106af3fd7e855331b26c0dbd9d369a
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-03-11 21:35:17 +0900

    [level 2] Title: 우박수열 정적분, Time: 5.41 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public double[] solution(int k, int[][] ranges) {
+        List<Integer> woobak = new ArrayList<>();
+        woobak.add(k);
+        
+        while(k > 1) {
+            if(k % 2 == 0) k /= 2;
+            else k = k * 3 + 1;
+            woobak.add(k);
+        }
+        
+        int n = woobak.size() - 1;
+        double[] areas = new double[n];
+        
+        for(int i = 0; i < n; i++) {
+            areas[i] = (woobak.get(i) + woobak.get(i + 1)) / 2.0;
+        }
+        
+        double[] answer = new double[ranges.length];
+        
+        for(int i = 0; i < ranges.length; i++) {
+            int start = ranges[i][0];
+            int end = n + ranges[i][1];
+            
+            if(start > end) {
+                answer[i] = -1.0;
+            } else if (start == end) {
+                answer[i] = 0.0;
+            } else {
+                double sum = 0;
+                for(int j = start; j < end; j++) {
+                    sum += areas[j];
+                }
+                answer[i] = sum;
+            }
+        }
+        
+        return answer;
+    }
+}
