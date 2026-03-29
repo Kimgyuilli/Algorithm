e8e55d54fb123ad2a4c4b43249f8be0834e0fa1a
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-03-29 21:21:25 +0900

    [level 2] Title: 숫자 블록, Time: 280.28 ms, Memory: 55.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(long begin, long end) {
+        int len = (int) (end - begin + 1);
+        int[] answer = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            long currentPos = begin + i;
+            answer[i] = (int) getBlockValue(currentPos);
+        }
+
+        return answer;
+    }
+
+    private long getBlockValue(long n) {
+        if (n == 1) return 0;
+
+        long maxDivisor = 1; 
+
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (n / i <= 10000000) {
+                    return n / i;
+                }
+                maxDivisor = i;
+            }
+        }
+        
+        return maxDivisor;
+    }
+}
