1e75d2abf37a210a00a6dac26ca10adfd48cb7e1
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2026-05-31 21:12:48 +0900

    Time: 26 ms (81.52%), Space: 107.5 MB (47.81%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,14 +1,15 @@
-import java.util.*;
-
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
-        int max = asteroids[asteroids.length-1];
-        for(int i : asteroids) {
-            if(mass < i) return false;
-            mass += i;
-            if(mass > max) return true;
+        long cur = mass;
+        long max = asteroids[asteroids.length - 1];
+
+        for (int asteroid : asteroids) {
+            if (cur < asteroid) return false;
+            cur += asteroid;
+            if (cur > max) return true;
         }
+
         return true;
     }
 }
