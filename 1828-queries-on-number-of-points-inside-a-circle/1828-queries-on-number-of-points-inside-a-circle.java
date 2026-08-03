95de4dcff49d8266e70aa5e715b9afa1259e7c65
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-08-04 07:58:45 +0900

    Time: 15 ms (94.2%), Space: 47.1 MB (48.41%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.java
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.java
@@ -3,11 +3,14 @@ class Solution {
         int n = queries.length;
         int[] result = new int[n];
         for(int i = 0; i < n; i++) {
-            int rr = queries[i][2]*queries[i][2];
+            int x = queries[i][0];
+            int y = queries[i][1];
+            int r = queries[i][2];
+            int rr = r*r;
 
             for(int[] point : points) {
-                int xd = queries[i][0] - point[0];
-                int yd = queries[i][1] - point[1];
+                int xd = x - point[0];
+                int yd = y - point[1];
                 int distance = xd * xd + yd * yd;
 
                 if(distance <= rr) {
