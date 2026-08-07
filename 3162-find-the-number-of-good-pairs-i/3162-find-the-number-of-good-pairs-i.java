cef896332d499092fd94269df6c5ceefb24e3713
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-08-07 17:31:35 +0900

    Time: 1 ms (100%), Space: 44.6 MB (66.34%) - LeetHub

---
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int answer = 0;
+        for(int num1 : nums1) {
+            for(int num2 : nums2) {
+                if(num1 % (num2 * k) == 0) answer++;
+            }
+        }
+        return answer;
+    }
+}
