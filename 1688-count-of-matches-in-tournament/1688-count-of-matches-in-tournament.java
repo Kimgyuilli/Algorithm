6f6102eb1b5805789e749725e3d47e67bc2fe77a
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-08-09 20:37:43 +0900

    Time: 0 ms (100%), Space: 42.3 MB (36.83%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int answer = 0;
+        while(n > 1) {
+            answer += n / 2;
+
+            if(n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n / 2 + 1;
+            }
+        }
+        return answer;
+    }
+}
