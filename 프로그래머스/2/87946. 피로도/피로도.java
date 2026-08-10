590ccf7dd93c58ce30c92f492b67f5d5b215072f
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-08-11 01:44:12 +0900

    [level 2] Title: 피로도, Time: 1.52 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,21 +1,33 @@
 class Solution {
-    int answer = 0;
+    private int[][] dungeons;
+    private int answer;
     
     public int solution(int k, int[][] dungeons) {
+        this.dungeons = dungeons;
+        this.answer = 0;
+        
         boolean[] visited = new boolean[dungeons.length];
-        dfs(dungeons, visited, k, 0);
+        
+        dfs(k, 0, visited);
+        
         return answer;
     }
     
-    private void dfs(int[][] dungeons, boolean[] visited, int piro, int depth){
-        answer = Math.max(answer, depth);
+    private void dfs(int k, int depth, boolean[] visited) {
         
-        for(int i = 0; i < dungeons.length; i++){
-            if(!visited[i] && piro >= dungeons[i][0]){
+        answer = Math.max(depth, answer);
+        
+        if(depth == dungeons.length) return;
+        
+        
+        for(int i = 0; i < dungeons.length; i++) {
+            if(!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
-                dfs(dungeons, visited, piro - dungeons[i][1], depth + 1);
+                dfs(k - dungeons[i][1], depth + 1, visited);
                 visited[i] = false;
             }
         }
+        
     }
+    
 }
