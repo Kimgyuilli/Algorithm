cd3feab59254fbd4c6d34e0bb33de44f063e6f8d
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-08-26 08:23:25 +0900

    Time: 1 ms (100%), Space: 46.7 MB (57.1%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int[] ans = new int[nums.size()];
+
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums.get(i);
+
+            if (num == 2) {
+                ans[i] = -1;
+                continue;
+            }
+
+            int bit = 1;
+
+            while ((num & bit) != 0) {
+                bit <<= 1;
+            }
+
+            ans[i] = num ^ (bit >> 1);
+        }
+
+        return ans;
+    }
+}
