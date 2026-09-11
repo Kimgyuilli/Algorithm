44b3ce618c1375246f6b41f36269c89f68908f39
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-09-11 18:22:55 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,38 @@
 class Solution {
-    static int answer;
-    static boolean[] visited;
+    private int[] parent;
     public int solution(int n, int[][] computers) {
+        this.parent = new int[n];
         
-        visited = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
         
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                answer++;
-                dfs(i, computers);
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(computers[i][j] == 1) union(i, j);
             }
+        }
+        
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            if(find(i) == i) answer++;
         }
         
         return answer;
     }
     
-    private void dfs(int n, int[][] computers){
-        visited[n] = true;
-        for(int i = 0; i < computers.length; i++){
-            if(!visited[i] && computers[n][i] == 1){
-                dfs(i, computers);
-            }
+    private void union(int node1, int node2) {
+        int rootA = find(node1);
+        int rootB = find(node2);
+        
+        if(rootA != rootB) {
+            parent[rootA] = rootB;
         }
     }
     
+    
+    private int find(int node) {
+        if(node == parent[node]) return node;
+        return parent[node] = find(parent[node]);
+    }
 }
