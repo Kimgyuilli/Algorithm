c62ad9df1f0662f5eac49c8e9ff2f8bd48e86756
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-09-11 18:49:41 +0900

    [level 3] Title: 정수 삼각형, Time: 5.06 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,14 +2,25 @@ class Solution {
     public int solution(int[][] triangle) {
         int n = triangle.length;
         
-        int[] dp = triangle[n-1].clone();
-        
-        for(int i = n-2; i >= 0; i--) {
+        for(int i = 1; i < n; i++) {
             for(int j = 0; j <= i; j++) {
-                dp[j] = Math.max(dp[j], dp[j+1]) + triangle[i][j];
+                if(j == 0) { 
+                    triangle[i][j] += triangle[i-1][j];
+                    continue;
+                } 
+                if(j == i) { 
+                    triangle[i][j] += triangle[i-1][j - 1];
+                    continue;
+                } 
+                triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j - 1]);
             }
         }
         
-        return dp[0];
+        int answer = -1;
+        
+        for(int i = 0; i < n; i++) {
+            answer = Math.max(answer, triangle[n-1][i]);
+        }
+        return answer;
     }
 }
