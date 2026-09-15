64277a01e5c1c7193f18574bb02eaef990935043
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-09-15 19:00:56 +0900

    [level 3] Title: 이중우선순위큐, Time: 40.61 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,38 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
+        Queue<Integer> decreasePq = new PriorityQueue<>(Comparator.reverseOrder());
+        Queue<Integer> increasePq = new PriorityQueue<>();
         
-        PriorityQueue<Integer> minPq = new PriorityQueue<>();
-        PriorityQueue<Integer> maxPq = new PriorityQueue<>(Comparator.reverseOrder());
-        
-        int add = 0;
-        int poll = 0;
+        int count = 0;
         for(String operation : operations) {
-            String[] operator = operation.split(" ");
-            int num = Integer.parseInt(operator[1]);
             
-            if(operator[0].equals("I")) {
-                minPq.offer(num);
-                maxPq.offer(num);
-                add++;
-            } else {
-                if(add - poll == 0) continue;
-                if(num > 0) {
-                    minPq.remove(maxPq.poll());
-                } else {
-                    maxPq.remove(minPq.poll());
-                }
-                poll++;
+            char oper = operation.charAt(0);
+            int num = Integer.parseInt(operation.split(" ")[1]);
+            
+            if(oper == 'I') {
+                increasePq.offer(num);
+                decreasePq.offer(num);
+                count++;
+                continue;
             }
+            
+            if(count == 0) continue;
+            if(num == 1) { 
+                increasePq.remove(decreasePq.poll());
+            }
+            else {
+                decreasePq.remove(increasePq.poll());   
+            }
+            count--;
+            
         }
         
-        if (add == poll) return new int[]{0, 0};
-        if (add - poll == 1) {
-            int val = minPq.peek();
-            return new int[]{val, val};
+        if(count == 0) return new int[] {0, 0};
+        if(count == 1) {
+            int val = increasePq.peek();
+            return new int[] {val, val};
         }
         
         
-        return new int[]{maxPq.peek(), minPq.peek()};
+        return new int[] {decreasePq.poll(), increasePq.poll()};
     }
 }
