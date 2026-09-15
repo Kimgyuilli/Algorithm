72dc6b7c791599de6f254f26d3875d843b3f3abd
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-09-15 19:46:16 +0900

    [level 3] Title: 숫자 게임, Time: 40.51 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -3,23 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] A, int[] B) {
         
-        int answer = 0;
-        
         Arrays.sort(A);
         Arrays.sort(B);
         
-        int bPointer = 0;
-        for(int emp : A) {
-            while(bPointer < B.length && B[bPointer] <= emp) {
-                bPointer++;
-            }
-            
-            if(bPointer < B.length) {
-                bPointer++;
-                answer++;
+        int len = A.length;
+        
+        int idxA = 0;
+        int idxB = 0;
+        int answer = 0;
+        
+        while(idxA < len && idxB < len) {
+            if(A[idxA] < B[idxB]) {
+                idxA++;
+                idxB++;
+            } else {
+                idxB++;
             }
         }
         
-        return answer;
+        return idxA;
     }
 }
