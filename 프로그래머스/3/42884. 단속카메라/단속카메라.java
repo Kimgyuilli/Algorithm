404d1b033e11ee9e7c2d68723df6b2be30cc159b
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-09-16 19:38:03 +0900

    [level 3] Title: 단속카메라, Time: 4.88 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,20 +1,22 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
         
         Arrays.sort(routes, (a, b) -> {
-           return a[1] - b[1]; 
+            return a[1]-b[1];
         });
         
+        
+        int checkpoint = routes[0][1];
         int answer = 1;
-        int camera = routes[0][1];
         
         for(int i = 1; i < routes.length; i++) {
-            if(routes[i][0] > camera) {
-                answer++;
-                camera = routes[i][1];
-            }
+            if(routes[i][0] <= checkpoint) continue;
+            
+            checkpoint = routes[i][1];
+            answer++;
+            
         }
         
         return answer;
