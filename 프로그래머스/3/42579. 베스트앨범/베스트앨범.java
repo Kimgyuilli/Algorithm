a280aa34e86f2add403547dfebf0205b1bc1f0a5
김규일 <87853959+Kimgyuilli@users.noreply.github.com>
2026-09-18 00:58:17 +0900

    [level 3] Title: 베스트앨범, Time: 2.97 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,35 +3,47 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         
-        Map<String, Integer> genres_list = new HashMap<>();
-        Map<String, List<int[]>> genres_map = new HashMap<>();
+        Map<String, Integer> genreTotal = new HashMap<>();
+        Map<String, List<int[]>> genreMap = new HashMap<>();
         
-        for(int i = 0; i < genres.length; i++) {
-            genres_list.put(genres[i], genres_list.getOrDefault(genres[i], 0) + plays[i]);
-            List<int[]> list = genres_map.getOrDefault(genres[i], new ArrayList<>());
-            list.add(new int[]{i, plays[i]});
-            genres_map.put(genres[i], list);
+        int len = genres.length;
+        
+        for(int i = 0; i < len; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+            
+            genreTotal.put(genre, genreTotal.getOrDefault(genre, 0) + play);
+            if(genreMap.get(genre) == null) {
+                genreMap.put(genre, new ArrayList<>());
+            }
+            genreMap.get(genre).add(new int[] {i, play});
         }
         
-        List<String> sorted_genres = new ArrayList<>(genres_list.keySet());
-        sorted_genres.sort((a, b) -> {
-            return genres_list.get(b) - genres_list.get(a);
+        List<Integer> answer = new ArrayList<>();
+        
+        
+        List<String> sorted_genre = new ArrayList<>(genreTotal.keySet());
+        sorted_genre.sort( (a, b) -> {
+           return genreTotal.get(b) - genreTotal.get(a);
         });
         
-        List<Integer> result = new ArrayList<>();
-        
-        for(String genre : sorted_genres) {
-            List<int[]> songs = genres_map.get(genre);
-            
-            songs.sort((a, b) -> b[1] - a[1]); 
+        for(String genre : sorted_genre) {
+            List<int[]> songs = genreMap.get(genre);
+            songs.sort((a, b) -> {
+               return b[1] - a[1]; 
+            });
             
             for(int i = 0; i < Math.min(2, songs.size()); i++) {
-                result.add(songs.get(i)[0]);
+                answer.add(songs.get(i)[0]);
             }
-            
         }
         
+        int[] result = new int[answer.size()];
         
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        for(int i = 0; i < answer.size(); i++) {
+            result[i] = answer.get(i);
+        }
+        
+        return result;
     }
 }
